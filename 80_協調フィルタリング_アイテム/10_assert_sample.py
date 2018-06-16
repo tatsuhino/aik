312ba5c565efaab8b1e57b7feacbfd51373c05ca
tatsuya hino <ronnosippo@gmail.com>
2018-06-16 14:00:39 +0900

    協調フィルタリングの実装

--- 80_協調フィルタリング_アイテム/10_assert_sample.py
+++ 80_協調フィルタリング_アイテム/10_assert_sample.py
@@ -48,7 +48,7 @@ def is_hit(model, user_id,item_id,item_list):
     logger.info("user_id:"+user_id+" buy_item_id:"+item_id+" predict_items:" + str(predict_item))
     if item_id in predict_item: return True
 
-def get_predict_list(trainset, similarities, n):
+def get_similarities_list(trainset, similarities, n):
     """
     データ加工用のfunction PageURL をキー、評価の降順にソートされた Taple のリストをValue として持つ辞書 を作成する
     """
@@ -68,8 +68,35 @@ def get_predict_list(trainset, similarities, n):
             data.update({raw_id2 : elem})
         results.update({raw_id1 : sorted(data.items(), key=lambda x: -x[1], reverse=True)[:n]})
 
-    print(results)
     return results
+
+# おすすめアイテムの上位N件を取得(購入直前のアイテム２つに対して、類似のアイテムを取得)
+def get_predict_item_top_n(test_data_line,sim_list, n):
+    buy_item = test_data_line.pop()
+    # 購入の直前に見たアイテムから操作する
+    test_data_line.reverse()
+
+    predict_dict = {}
+    predict_dict["exceed_sim_vecs"] = []
+    predict_dict["sim_vecs"] = []
+    for item in test_data_line :
+        if item == buy_item: continue
+        try:
+            predict_item = sim_list[item]
+            print(predict_item)
+            predict_dict["exceed_sim_vecs"].extend(predict_item)
+            predict_dict["sim_vecs"].extend(predict_item[:int(n/2 + n%2)]) # TODO 直近２件決め打ちなロジック
+            if len(predict_dict["exceed_sim_vecs"]) >= (n*2): break
+        except KeyError: continue # ボキャブラリーに該当itemが存在しない場合
+    
+    # 予想アイテム数がnを超える前にループを抜けた場合は、exceed_sim_vecsから持ってくる
+    if len(predict_dict["sim_vecs"]) < n:
+        predict_dict["sim_vecs"].extend(predict_dict["exceed_sim_vecs"])
+    if len(predict_dict["sim_vecs"]) == 0 : return [] 
+
+    all_predict_item = [vec[0] for vec in predict_dict["sim_vecs"]]
+    return list(set(all_predict_item))[:n]
+
 
 def export_format_data(all_data_before):
     all_data = []
@@ -90,7 +117,6 @@ def export_format_data(all_data_before):
     return train_tmp_file
 
 def main():
-
     all_data = []
     with open(BASE_DIR + './events.csv_converted', mode='r') as f:
         lines = f.readlines()
@@ -118,15 +144,17 @@ def main():
         similarities = model.compute_similarities()
         logger.info("モデル学習完了")
         
-        predict_list = get_predict_list(trainset, similarities,10)
-        logger.info(predict_list)
-        # TODO １ユーザに対して最大でもX回までしか評価しないようにtestセットから除く
+        sim_list = get_similarities_list(trainset, similarities,10)
+        logger.info(sim_list)
         hit_count=0
         for test_data in test_data_list:
             for v in test_data.values() : line_value = v
-            buy_item_id = line_value.split(",")[1].split(" ")[-1]
-            pre_buy_item_id = line_value.split(",")[1].split(" ")[-2]
-            if buy_item_id in predict_list[pre_buy_item_id] : hit_count += 1
+            test_line = line_value.split(",")[1].split(" ")
+            buy_item_id = test_line[-1]
+            # pre_buy_item_id = line_value.split(",")[1].split(" ")[-2]
+
+            predict_list = get_predict_item_top_n(test_line,sim_list,10)
+            if buy_item_id in predict_list : hit_count += 1
 
         # 評価の印字
         test_buy_item_count = len(test_data_list)
@@ -135,15 +163,12 @@ def main():
         if test_buy_item_count!=0 : 
             logger.info("[ヒット率]" + str(hit_count / test_buy_item_count))
 
-        return 
-
-
-    
+        return
         
 if __name__ == "__main__":
     init_logger()
     start = time.time()
     logger.info("[START]------------------------------------------------")
     main()
-   logger.info("[TIME]:{0:.2f}".format(time.time() - start) + "(sec)")
+    logger.info("[TIME]:{0:.2f}".format(time.time() - start) + "(sec)")
     logger.info("[END]--------------------------------------------------")
